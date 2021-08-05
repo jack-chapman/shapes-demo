f8aa6bdf8f57f3dcd697dd79c9e49fc5e6eb8284
jack-chapman <jackchapman093@gmail.com>
2021-08-05 22:44:14 +0100

    Fix CSS type errors

--- src/components/Shape.vue
+++ src/components/Shape.vue
@@ -3,21 +3,21 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, PropType } from 'vue'
+import { computed, defineComponent, PropType } from 'vue';
 
-type TopBottom = 'top' | 'bottom'
-type RightLeft = 'right' | 'left'
+type TopBottom = 'top' | 'bottom';
+type RightLeft = 'right' | 'left';
 
-export type CurvePosition = `${TopBottom}-${RightLeft}`
+export type CurvePosition = `${TopBottom}-${RightLeft}`;
 
-export type ShapeColour = 'red' | 'emerald' | 'lapis' | 'charcoal' | 'white'
+export type ShapeColour = 'red' | 'emerald' | 'lapis' | 'charcoal' | 'white';
 
 export default defineComponent({
   props: {
     x: Number,
     y: Number,
     curve: String as PropType<CurvePosition>,
-    colour: String as PropType<ShapeColour>
+    colour: String as PropType<ShapeColour>,
   },
   setup(props) {
     const curveClass = computed(() => {
@@ -25,31 +25,31 @@ export default defineComponent({
         'rounded-tr-full': props.curve === 'top-right',
         'rounded-tl-full': props.curve === 'top-left',
         'rounded-br-full': props.curve === 'bottom-right',
-        'rounded-bl-full': props.curve === 'bottom-left'
-      }
-    })
+        'rounded-bl-full': props.curve === 'bottom-left',
+      };
+    });
     const colourClass = computed(() => {
       return {
         'bg-red-500': props.colour === 'red',
         'bg-emerald-500': props.colour === 'emerald',
         'bg-indigo-500': props.colour === 'lapis',
         'bg-true-gray-800': props.colour === 'charcoal',
-        'bg-white': props.colour === 'white'
-      }
-    })
+        'bg-white': props.colour === 'white',
+      };
+    });
     const styles = computed(() => {
       return {
-        'grid-row': props.y,
-        'grid-column': props.x
-      }
-    })
+        gridRow: props.y,
+        gridColumn: props.x,
+      };
+    });
     return {
       classes: {
         ...curveClass.value,
-        ...colourClass.value
+        ...colourClass.value,
       },
-      styles
-    }
+      styles,
+    };
   },
-})
+});
 </script>
--- src/components/ShapeGrid.vue
+++ src/components/ShapeGrid.vue
@@ -5,27 +5,27 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue';
 
 export default defineComponent({
   props: {
     cols: Number,
     rows: Number,
-    size: Number
+    size: Number,
   },
   setup(props) {
-    const rows = computed(() => `repeat(${props.rows}, ${props.size}px)`)
-    const cols = computed(() => `repeat(${props.cols}, ${props.size}px)`)
+    const rows = computed(() => `repeat(${props.rows}, ${props.size}px)`);
+    const cols = computed(() => `repeat(${props.cols}, ${props.size}px)`);
 
     const style = computed(() => {
       return {
-        'grid-template-rows': rows.value,
-        'grid-template-columns': cols.value
-      }
-    })
+        gridTemplateRows: rows.value,
+        gridTemplateColumns: cols.value,
+      };
+    });
     return {
-      style
-    }
+      style,
+    };
   },
-})
+});
 </script>
